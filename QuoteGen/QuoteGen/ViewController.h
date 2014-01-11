08763cc0c859ee883e994a27ed22dd4ce00b9ecf
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2014-01-11 17:38:45 +0000

    DEV: 1st display

--- QuoteGen/QuoteGen/ViewController.h
+++ QuoteGen/QuoteGen/ViewController.h
@@ -10,4 +10,11 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, strong) NSArray *myQuotes;
+
+@property (nonatomic, strong) NSMutableArray *movieQuotes;
+@property (nonatomic, strong) IBOutlet UITextView *quoteText;
+
+- (IBAction)quoteButtonTapped:(id)sender;
+
 @end
