5756361fbf7d76039d301e35ff0c154862cc396a
Alexandre Humeau <alexandre.humeau@student.ecp.fr>
2014-01-13 20:36:37 +0000

    DEV: Iteracting through the property list

--- QuoteQuiz/QuoteQuiz/Quiz.h
+++ QuoteQuiz/QuoteQuiz/Quiz.h
@@ -10,8 +10,17 @@
 
 @interface Quiz : NSObject
 
-@property (nonatomic, strong) NSMutableArray * movieArray;
+@property (nonatomic, retain) NSMutableArray * movieArray; // Previous Property
+@property (nonatomic, assign) NSInteger correctCount;
+@property (nonatomic, assign) NSInteger incorrectCount;
+@property (nonatomic, assign) NSInteger quizCount;
+@property (nonatomic, readonly, strong) NSString * quote;
+@property (nonatomic, readonly, strong) NSString * ans1;
+@property (nonatomic, readonly, strong) NSString * ans2;
+@property (nonatomic, readonly, strong) NSString * ans3;
 
-- (id)initWithQuiz:(NSString*)plistName;
+-(id) initWithQuiz:(NSString*)plistName; // Previous Method
+-(void) nextQuestion: (NSUInteger) idx;
+-(BOOL) checkQuestion: (NSUInteger) question forAnswer: (NSUInteger) answer;
 
 @end
